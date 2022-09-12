bcbe21812e7dbff816af591876f8abbd96c72808
_Anna Sabinina_(student) <49241@cv.k12.ca.us>
2022-09-12 16:59:38 -0700

    Initial commit

---
+++ Training_Functions.java
@@ -0,0 +1,18 @@
+public class Training_Functions {
+    public static void dosmth() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        int j = 0;
+                while (j<5){
+                    System.out.println(j);
+                    j++;
+                }
+                String currLine = "#";
+    }
+}
